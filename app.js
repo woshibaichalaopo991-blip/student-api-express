f95f88399ea2ff3ec94aa1913c527dfce94b93d6
Nyein Su Thinzar <woshibaichalaopo991@gmail.com>
2026-09-07 11:29:50 +0700

    Middleware& Render Page

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const express= require("express");
-const students=require("./students.json");
-const app=express();
-const PORT=3000;
+const express = require("express");
+const students = require("./students.json");
+const app = express();
+const PORT = 3000;
+app.set("view engine","ejs");
+app.use((req, res, next) => {
+    const time = new Date().toLocaleTimeString();
+console.log(`${req.method} ${req.url} ${time}`);
+next();
+});
+app.use(express.json());
+
 app.get("/", (req, res) => {
 res.send(`
 <h1>Student API in Express</h1>
@@ -10,9 +18,15 @@ res.send(`
 <ul>
 <li>GET /api/students</li>
 <li>GET /api/students/1</li>
-<li>GET /api/students?major=IT</li>
+<li>GET /students</li>
+<li>POST /api/students</li>
 </ul>
 `);
+});
+app.use((req, res, next) => {
+    const time = new Date().toLocaleTimeString();
+console.log(`${req.method} ${req.url} ${time}`);
+next();
 });
 app.get("/api/students", (req, res) => {
 const major = req.query.major;
@@ -22,16 +36,32 @@ return res.json(filteredStudents);
 }
 res.json(students);
 });
+app.post("/api/students", (req, res) => {
+const newStudent = req.body;
+students.push(newStudent);
+res.status(201).json(newStudent);
+});
 app.get("/api/students/:id", (req, res) => {
 const id = req.params.id;
 const student = students.find((s) => s.id === Number(id));
 if (!student) {
-    return res.status(404).json({
+return res.status(404).json({
 error: "Student not found"
 });
 }
 res.json(student);
 });
-app.listen(PORT,()=>{
-    console.log(`Running on http://localhost:${PORT}`);
+app.get("/students", (req, res) => {
+res.render("students", {
+title: "All Students",
+students: students
+});
+});
+app.use((req, res) => {
+res.status(404).json({
+error: "Route not found"
+});
+});
+app.listen(PORT, () => {
+console.log(`Running on http://localhost:${PORT}`);
 });
